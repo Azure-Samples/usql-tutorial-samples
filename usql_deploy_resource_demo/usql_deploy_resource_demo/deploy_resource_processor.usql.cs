dc24978d5000a09a117435f2a38319ac78470594
Saveen Reddy <saveenr@live.com>
2017-07-10 15:04:52 -0700

    Initial check-in

---
+++ usql_deploy_resource_demo/usql_deploy_resource_demo/deploy_resource_processor.usql.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+    [SqlUserDefinedProcessor]
+    public class HelloWorldProcessor : IProcessor
+    {
+        private string hw;
+
+        public HelloWorldProcessor()
+        {
+            this.hw = System.IO.File.ReadAllText("helloworld.txt");
+        }
+
+        public override IRow Process(IRow input, IUpdatableRow output)
+        {
+            output.Set<int>("DepID", input.Get<int>("DepID"));
+            output.Set<string>("DepName", input.Get<string>("DepName"));
+            output.Set<string>("HelloWorld", hw);
+            return output.AsReadOnly();
+        }
+    }
+}
