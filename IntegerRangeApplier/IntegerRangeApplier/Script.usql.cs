0c5ff227de8e180f45d54e978f22993e1003d6e7
Saveen Reddy <saveenr@live.com>
2017-07-31 00:27:03 -0700

    Added sample

---
+++ IntegerRangeApplier/IntegerRangeApplier/Script.usql.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+
+    [SqlUserDefinedApplier]
+    public class IntegerRangeApplier : IApplier
+    {
+        private int Start;
+        private int End;
+
+        public IntegerRangeApplier(int start, int end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public override IEnumerable<IRow> Apply(IRow input, IUpdatableRow output)
+        {
+            for (int i = this.Start; i <= this.End; i++)
+            {
+                output.Set<int>("Value", i);
+                yield return output.AsReadOnly();
+            }
+        }
+    }
+}
